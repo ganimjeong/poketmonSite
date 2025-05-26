f2d21222d1a5cd104806bfa3fd6c5084bb3f80b5
ganimjeong <86656605+ganimjeong@users.noreply.github.com>
2025-05-26 22:14:53 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,27 @@
 // [기본 리액트식]
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import App from './App.jsx'
-// import { BrowserRouter } from 'react-router-dom'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </StrictMode>,
-// )
-
-// [github Page배포용]
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 )
+
+// // [github Page배포용]
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.jsx'
+// import { HashRouter } from 'react-router-dom'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <HashRouter>
+//       <App />
+//     </HashRouter>
+//   </StrictMode>,
+// )
