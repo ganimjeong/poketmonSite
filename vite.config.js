83b05307b707b760b439841b7b000a13be91a5e3
ganimjeong <86656605+ganimjeong@users.noreply.github.com>
2025-05-26 22:05:46 +0900

    Update vite.config.js for Deploy as GithubPages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
+//[기본 리액트]
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+//[github pages]
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/poketmonSite/', // ←GitHub 저장소 이름으로
 })
