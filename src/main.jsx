41c6ff2fa967f40379d3bc557b27772afc1cae81
ganimjeong <86656605+ganimjeong@users.noreply.github.com>
2025-05-26 22:00:07 +0900

    Update main.jsx for Deploy as GithubPages
    BrowserRouter to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,27 @@
+// [기본 리액트식]
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.jsx'
+// import { BrowserRouter } from 'react-router-dom'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </StrictMode>,
+// )
+
+// [github Page배포용]
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
